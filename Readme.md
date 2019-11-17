ef8e1241ca3a9ba75c9d927af1a0dda9d73f3022
malverndongeni <malvern.dongeni@econet.co.zw>
2019-11-17 13:21:58 +0200

    project setup

---
+++ Readme.md
@@ -0,0 +1,6 @@
+##### Simple Online Shop
+learning containers and a bit of PHP.
+
+This projects demonstrates a simple backend application that retrieves hardcoded stock items and display them.
+
+
---
+++ src/main/java/zw/co/malv3rn/SimpleShopApplication.java
@@ -0,0 +1,13 @@
+package zw.co.malv3rn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShopApplication.class, args);
+	}
+
+}
